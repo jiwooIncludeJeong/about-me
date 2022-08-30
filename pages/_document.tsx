0eb1cef573f1152c7ada9e5bbb0fadad139b0227
jeongjiwoo-include <jjw89506343@gmail.com>
2022-08-31 00:11:57 +0900

    [develop] resolve favicon locaiton

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel={'icon'} href={'/favicon.ico'} />
+          <link rel={'icon'} href={'/about-me/favicon.ico'} />
         </Head>
         <body>
           <Main />
