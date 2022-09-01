eaa66118c62ae2b3c9335e11b71ba30e226a0b6e
jiwooIncludeJeong <jiwooIncludeJeong@users.noreply.github.com>
2022-09-01 15:41:30 +0000

    deploy: 35ecd8ee39cce17bade13900609fe38a8f626114

---
+++ _next/static/AgaQ0VAGFM2opLgd8Z9kx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/915-ad9d787e82f0321c.js","static/chunks/pages/index-334c1ef064eb38b2.js"],"/_error":["static/chunks/pages/_error-d6e4ec1a3c3fdc31.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
