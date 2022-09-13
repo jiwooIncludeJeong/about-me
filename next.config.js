b4a5f8ab2ea2af609e0a3f54fa245812e90d3107
jeongjiwoo-include <jjw89506343@gmail.com>
2022-09-13 17:39:28 +0900

    [deploy] resolve font issue by adding assetPrefix 2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  assetPrefix:isProduction? '/about-me' : undefined,
+  assetPrefix:isProduction? 'https://jiwooincludejeong.github.io/about-me/' : undefined,
   reactStrictMode: true,
   swcMinify: true,
   extends: [
