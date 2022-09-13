52c70b68fb16aebd16e39f6c92cf6ac9effda773
jeongjiwoo-include <jjw89506343@gmail.com>
2022-09-13 17:10:26 +0900

    [deploy] resolve font not applying at other browser

--- styles/global-styles.ts
+++ styles/global-styles.ts
@@ -5,7 +5,7 @@ export const GlobalStyles = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
-    font-family:"Pretendard", "Roboto",-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    font-family: "Pretendard", "Roboto", "Magazine", -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
 
